3d17d8c7dc765f3ee41989b717d3858460ea174a
Devin McCabe <dmccabe@broadinstitute.org>
2024-03-20 13:47:32 -0400

    (un)serialize pd.Timestamp values

--- dataframe-json-packing/tests/serialization_test.py
+++ dataframe-json-packing/tests/serialization_test.py
@@ -1,7 +1,10 @@
-from dataframe_json_packing import pack, unpack
-import pandas as pd
 import datetime
 import json
+import re
+
+import pandas as pd
+
+from dataframe_json_packing import pack, unpack
 
 
 def test_df_to_dict():
@@ -11,18 +14,44 @@ def test_df_to_dict():
             "int": [1, 2, None],
             "float": [1.1, 2.2, None],
             "d": [datetime.date(2000, 1, 1), datetime.date(2001, 2, 2), None],
+            "dt": [
+                pd.to_datetime(datetime.datetime(2000, 1, 1, hour=1)),
+                pd.to_datetime(datetime.datetime(2001, 2, 2, hour=2)),
+                None,
+            ],
+            "dt_tz": [
+                pd.to_datetime(datetime.datetime(2000, 1, 1, hour=1), utc=True),
+                pd.to_datetime(datetime.datetime(2001, 2, 2, hour=2), utc=True),
+                None,
+            ],
         }
     ).convert_dtypes()
 
     packed = pack(df)
+
     # make sure missing values are represented with "null" and not NaN
-    "NaN" not in json.dumps(packed)
+    assert re.search(r"NaN|NaT", json.dumps(packed)) is None
+
     assert packed == {
         "columns": [
             {"name": "string", "type": "string", "values": ["a", "b", None]},
             {"name": "int", "type": "int", "values": [1, 2, None]},
             {"name": "float", "type": "float", "values": [1.1, 2.2, None]},
             {"name": "d", "type": "date", "values": [730120, 730518, None]},
+            {
+                "name": "dt",
+                "type": "datetime64",
+                "values": ["2000-01-01 01:00:00", "2001-02-02 02:00:00", None],
+            },
+            {
+                "name": "dt_tz",
+                "type": "datetime64",
+                "values": [
+                    "2000-01-01 01:00:00+00:00",
+                    "2001-02-02 02:00:00+00:00",
+                    None,
+                ],
+            },
         ]
     }
 
